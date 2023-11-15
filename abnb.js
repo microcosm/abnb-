9973e5f0cbf17e472a977695577afefe0654408e
Andrew McWilliams <andy@jahya.net>
2023-11-15 11:44:54 -0500

    Initial commit

---
+++ abnb.js
@@ -0,0 +1,79 @@
+function getSpreadsheetConfig() {
+  return {
+    name: 'abnb—',
+    id: '1XLH1PWZcehjUo1_DjnzeRQUbB8Z0JLryD_vu6gNNOPw'
+  };
+}
+
+function getValuesSheetConfig() {
+  return {
+    name: 'Values',
+    range: 'B5:C9',
+    usersColumnIndex: 0,
+    eventsCalendarIdRowIndex: 0,
+    eventsCalendarIdColumnIndex: 1
+  };
+}
+
+function getFeatureSheetConfigs() {
+  return [
+    this.getMaintenanceConfig()
+  ];
+}
+
+function getMaintenanceConfig() {
+  const sections = ['titles', 'titlesAboveBelow', 'headers', 'main', 'underMain', 'rowsOutside', 'columnsOutside'];
+
+  return {
+    name: 'Maintenance',
+    features: {
+      orderSheetSections: {
+        events: [Event.onOvernightTimer],
+        sections: SectionsCategory.MAIN,
+        order: [
+          { column: 'M', direction: 'ascending' },
+          { column: 'I', direction: 'ascending' },
+          { column: 'H', direction: 'ascending' },
+          { column: 'B', direction: 'ascending' },
+          { column: 'C', direction: 'ascending' }
+        ]
+      }
+    },
+    sidebar: {
+      heading: {
+        type: 'heading',
+        title: 'Maintenance'
+      },
+      arrange: {
+        type: 'buttons',
+        title: 'Arrange by',
+        options: ['Next Due', 'Locale', 'Schedule'],
+        features: {
+          orderSheetSections: {
+            events: [Event.onSidebarSubmit],
+            sections: SectionsCategory.MAIN,
+            priority: 'HIGH_PRIORITY',
+            by: {
+              nextDue: [
+                { column: 'M', direction: 'ascending' },
+                { column: 'I', direction: 'ascending' },
+                { column: 'H', direction: 'ascending' },
+                { column: 'B', direction: 'ascending' },
+                { column: 'C', direction: 'ascending' }
+              ],
+              locale: [
+                { column: 'B', direction: 'ascending' },
+                { column: 'C', direction: 'ascending' }
+              ],
+              schedule: [
+                { column: 'K', direction: 'ascending' },
+                { column: 'B', direction: 'ascending' },
+                { column: 'C', direction: 'ascending' }
+              ]
+            }
+          }
+        }
+      }
+    }
+  };
+}
